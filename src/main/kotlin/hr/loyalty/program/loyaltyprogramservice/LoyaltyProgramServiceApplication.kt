353272a6ba432e9d8e6e11fb3c12da94d9540c5a
Marko Jerkić <markojerkic266@gmail.com>
2022-02-10 21:20:51 +0100

    initial commit

---
+++ src/main/kotlin/hr/loyalty/program/loyaltyprogramservice/LoyaltyProgramServiceApplication.kt
@@ -0,0 +1,11 @@
+package hr.loyalty.program.loyaltyprogramservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoyaltyProgramServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<LoyaltyProgramServiceApplication>(*args)
+}
